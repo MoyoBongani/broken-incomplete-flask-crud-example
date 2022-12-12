6b85ae44e55aad0ac339a895ae98ebc60ca38036
MoyoBongani <98457438+MoyoBongani@users.noreply.github.com>
2022-12-12 23:15:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,68 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 import json
+import os
 mysql = MySQL()
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 CORS(app)
+
 # My SQL Instance configurations
 # Change these details to match your instance configurations
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = '34.142.43.53'
+app.config['MYSQL_HOST'] = 'localhost'
 mysql.init_app(app)
 
-@app.route("/add") #Add Student
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+# This is the home / index page (How to make this our starting point???)
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    return render_template("index.html")
+
+# Add student
+@app.route("/add", methods=['GET', 'POST']) #Add Student
 def add():
-  name = request.args.get('name')
-  email = request.args.get('email')
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email) # kludge - use stored proc or params
-  cur.execute(s)
-  mysql.connection.commit()
+    if request.method == "POST":
+        name = request.form['studentName']
+        email = request.form['email']
+        cursor = mysql.connection.cursor() #create a connection to the SQL instance
+        cursor.execute('''INSERT INTO students (studentName, email) VALUES(%s,%s)''',(name,email)) # execute
+        mysql.connection.commit()
+        cursor.close()
+        return render_template("success.html")
 
-  return '{"Result":"Success"}' # Really? maybe we should check!
-
-@app.route("/delete") #Delete Student
+# Delete student
+@app.route("/delete", methods=['GET', 'POST']) #Delete Student
 def delete():
-  name = request.args.get('name')
-  email = request.args.get('email')
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  s='''DELETE FROM students WHERE studentName = %s''',(name) # kludge - use stored proc or params
-  cur.execute(s)
-  mysql.connection.commit()
+    if request.method == "POST":
+        ID = request.form['ID']
+        #name = request.form['studentName']
+        cursor = mysql.connection.cursor() #create a connection to the SQL instance
+        cursor.execute('''DELETE FROM students WHERE studentID=%s''', (ID,)) # execute
+        #cursor.execute('''DELETE FROM students WHERE studentName=%s''', (name,)) # execute
+        mysql.connection.commit()
+        cursor.close()
+        return render_template("success.html")
 
-  return '{"Result":"Success"}' # Really? maybe we should check!
-  
-@app.route("/") #Default - Show Data
+
+# Change to add form html
+@app.route('/addPage', methods=['GET', 'POST'])
+def addPage():
+    return render_template("add.html")
+
+# Change to delete form html
+@app.route('/deletePage', methods=['GET', 'POST'])
+def deletePage():
+    return render_template("delete.html")
+
+
+@app.route("/read", methods=['GET', 'POST']) #Default - Show Data
 def read(): # Name of the method
   cur = mysql.connection.cursor() #create a connection to the SQL instance
   cur.execute('''SELECT * FROM students''') # execute an SQL statment
@@ -54,9 +80,8 @@ def read(): # Name of the method
     status=200,
     mimetype='application/json'
   )
-  return ret #Return the data in a string format
-if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
-  #app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
-    
+  return ret
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
+#app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
